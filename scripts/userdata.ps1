fa66251fd27c8eb1f6c60b1f439c3e634669ba8f
Ian Relf <ian.relf@digital.homeoffice.gov.uk>
2023-05-16 10:58:23 +0100

    First work out if host has joined a Domain or is still part of Workgroup.

--- scripts/userdata.ps1
+++ scripts/userdata.ps1
@@ -1,24 +1,66 @@
 Start-Transcript -path C:\PerfLogs\userdata_output.log -append
 
-Write-Host 'Getting IP address of host!'
-$myip = Get-Netipaddress -addressfamily ipv4
+Write-Host "Checking if host joined to a domain, yet"
+$is_part_of_domain = (Get-WmiObject -Class Win32_ComputerSystem).PartOfDomain
+$workgroup = (Get-WmiObject -Class Win32_ComputerSystem).Workgroup
+$is_part_of_workgroup = $workgroup -eq "WORKGROUP"
+$is_part_of = ""
+$is_part_of_valid = $false
 
-$firstoctate = $myip[0].ipaddress.Substring(0,4)
-$secondoctate = $myip[0].ipaddress.Substring(5,4)
-
-Write-Host $firstoctate
-Write-Host $secondoctate
-
-Write-Host 'Deciphering the Environment!'
-if($firstoctate -eq "10.8")
+if ($is_part_of_domain -eq $true -and $is_part_of_workgroup -eq $false)
 {
-$environment = "notprod"
+    $is_part_of = "DOMAIN"
+    $is_part_of_valid = $true
+}
+elseif ($is_part_of_workgroup -eq $true -and $is_part_of_domain -eq $false)
+{
+    $is_part_of = "WORKGROUP"
+    $is_part_of_valid=$true
+}
+elseif ($is_part_of_domain -eq $true -and $is_part_of_workgroup -eq $true)
+{
+    $is_part_of = "BOTH"
+    Write-Host "ERROR! The host appears to be part of a DOMAIN AND a WORKGROUP!"
+}
+elseif (-not $is_part_of_domain -eq $true -and -not $is_part_of_workgroup -eq $true)
+{
+    $is_part_of = "NEITHER"
+    Write-Host "ERROR! The host appears to be neither part of a DOMAIN NOR part of a WORKGROUP!"
+}
+else
+{
+    $is_part_of = "ERROR"
+    Write-Host "ERROR! Cannot work out if host is part of a DOMAIN or part of a WORKGROUP!"
+}
+
+if ($is_part_of_valid)
+{
+    Write-Host "Host is part of $is_part_of"
+}
+else
+{
+    Write-Host "DEBUG! is_part_of_domain = $is_part_of_domain, is_part_of_workgroup = $is_part_of_workgroup"
+}
+
+Write-Host 'Getting IP address of host!'
+$my_ip_full = Get-Netipaddress -addressfamily ipv4
+$my_ip = $my_ip_full[0].ipaddress
+Write-Host "IP address of host = $my_ip"
+
+$octets = $my_ip -split "\."
+$subnet = $octets[0] + "." + $octets[1]
+
+
+Write-Host "Deciphering the Environment from subnet $subnet"
+if($subnet -eq "10.8")
+{
+$environment = "NotProd"
 
 }
 
-elseif($firstoctate -eq "10.2")
+elseif($subnet -eq "10.2")
 {
-$environment = "prod"
+$environment = "Prod"
 
 }
 
