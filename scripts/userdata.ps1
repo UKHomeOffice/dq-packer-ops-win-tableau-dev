e7ed8d2bcd56c571625112d92cc55044a7d4a329
Ian Relf <ian.relf@digital.homeoffice.gov.uk>
2023-05-16 13:17:49 +0100

    Then figure out environment and desired hostname. Then rename host and join to Domain.

--- scripts/userdata.ps1
+++ scripts/userdata.ps1
@@ -1,5 +1,6 @@
 Start-Transcript -path C:\PerfLogs\userdata_output.log -append
 
+# First work out if host has joined a Domain or is still part of Workgroup
 Write-Host "Checking if host joined to a domain, yet"
 $is_part_of_domain = (Get-WmiObject -Class Win32_ComputerSystem).PartOfDomain
 $workgroup = (Get-WmiObject -Class Win32_ComputerSystem).Workgroup
@@ -42,68 +43,98 @@ else
     Write-Host "DEBUG! is_part_of_domain = $is_part_of_domain, is_part_of_workgroup = $is_part_of_workgroup"
 }
 
+# Get the IP Address and break it down into managable parts
 Write-Host 'Getting IP address of host!'
 $my_ip_full = Get-Netipaddress -addressfamily ipv4
 $my_ip = $my_ip_full[0].ipaddress
 Write-Host "IP address of host = $my_ip"
 
 $octets = $my_ip -split "\."
-$subnet = $octets[0] + "." + $octets[1]
+$subnet_part = $octets[0] + "." + $octets[1]
+$host_part = $octets[2] + "." + $octets[3]
 
 
-Write-Host "Deciphering the Environment from subnet $subnet"
-if($subnet -eq "10.8")
+# Try to figure out the Environment from the IP address
+Write-Host "Deciphering the Environment from subnet part of the IP Address $subnet_part"
+if($subnet_part -eq "10.8")
 {
-$environment = "NotProd"
-
+    $environment = "NotProd"
 }
-
-elseif($subnet -eq "10.2")
+elseif($subnet_part -eq "10.2")
 {
-$environment = "Prod"
-
+    $environment = "Prod"
 }
-
-else {
-$environment = "test"
-
+elseif ($octets[0] -eq "172")
+{
+    # When Packer is building the Instance (or copying the AMI) the IP address starts 172.
+    # No point in trying to do anything clever in this userdata script yet
+    $environment = "Building..."
 }
-
+else
+{
+    $environment = "UNKNOWN"
+}
 Write-Host ">>>>>>>>>>> Environment is $environment! <<<<<<<<<<<<<"
 
-Write-Host 'Deciphering the Bastion!'
-if($secondoctate -eq "6.10")
+
+# Try to figure out the host from the IP Address
+$current_hostname = $env:computername
+Write-Host "The current hostname is $current_hostname"
+if ($environment -eq "NotProd" -or $environment -eq "Prod")
 {
-$bastion = "DEVELOPMENT"
-
+    Write-Host "Deciphering the desired name of the host from the host part of the IP Address $host_part !"
+    if($host_part -eq "6.10")
+    {
+        $new_hostname = "TAB-DEP-1"
+    }
+    elseif($host_part -eq "6.11")
+    {
+        $new_hostname = "TAB-DEP-2"
+    }
+    elseif($host_part -eq "6.12")
+    {
+        $new_hostname = "TAB-DEP-3"
+    }
+    elseif($host_part -eq "6.15")
+    {
+        $new_hostname = "TAB-DEP-2019"
+    }
+    else
+    {
+        $new_hostname = "TAB-DEP-$octets[3]"
+    }
+    Write-Host ">>>>>>>>>>> Host should be named $new_hostname <<<<<<<<<<<<<"
 }
 
-elseif($secondoctate -eq "6.11")
-{
-$bastion = "DEPLOYMENT"
-
-}
-
-else {
-$bastion = "TABLEAU-DEV+"
-
-}
-
-Write-Host ">>>>>>>>>>> Host is $bastion <<<<<<<<<<<<<"
 
 Write-Host 'Adding bucket variable'
 [Environment]::SetEnvironmentVariable("S3_OPS_CONFIG_BUCKET", "s3-dq-ops-config-$environment/sqlworkbench", "Machine")
 [System.Environment]::SetEnvironmentVariable('S3_OPS_CONFIG_BUCKET','s3-dq-ops-config-$environment/sqlworkbench')
 
-Write-Host 'Join System to the DQ domain'
-$joiner_pw = (Get-SSMParameter -Name "AD_AdminPasswordd" -WithDecryption $True).Value
-$domain = 'dq.homeoffice.gov.uk'
-$username = 'dq\domain_joiner'
-$password = ConvertTo-SecureString $joiner_pw -AsPlainText -Force
-$credential = New-Object System.Management.Automation.PSCredential($username,$password)
+# Rename Computer and Join to Domain
+if ($is_part_of_domain -eq $false -and $is_part_of_valid -eq $true)
+{
+    Write-Host 'Join System to the DQ domain'
+    $joiner_usr = (Get-SSMParameter -Name "AD_Domain_Joiner_Username" -WithDecryption $False).Value
+    $joiner_pwd = (Get-SSMParameter -Name "AD_Domain_Joiner_Password" -WithDecryption $True).Value
+    $domain = 'dq.homeoffice.gov.uk'
+    $username = $joiner_usr + "@" + $domain
+    $password = ConvertTo-SecureString $joiner_pwd -AsPlainText -Force
+    $credential = New-Object System.Management.Automation.PSCredential($username,$password)
 
-Rename-Computer -NewName $bastion
-sleep 20
-Add-Computer -DomainName $domain -Options JoinWithNewName,AccountCreate -Credential $credential -restart -force
+    if ($current_hostname -ne $new_hostname)
+    {
+        Write-Host "Renaming host from $current_hostname to $new_hostname"
+        Rename-Computer -NewName $new_hostname
+        sleep 20
+        Write-Host "Joining host to Domain $domain - with rename option"
+        Add-Computer -DomainName $domain -Credential $credential -Options JoinWithNewName -NewName $new_hostname -Restart -Force
+    }
+    else
+    {
+        Write-Host "Joining host to Domain $domain - without rename option"
+        Add-Computer -DomainName $domain -Credential $credential -Restart -Force
+    }
+}
 
 Stop-Transcript
