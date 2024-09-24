9a4ca9b2e63a608c86fb097ad6a43c6a89218194
D-Peter Miller <DEV\d-millerp2@a-suvlegx7z0xs.dev.dacc.phz>
2024-09-24 11:31:01 +0100

    Extending timeout for WinRM

--- scripts/setupwrm.ps1
+++ scripts/setupwrm.ps1
@@ -22,7 +22,7 @@ write-output "Setting up WinRM"
 write-host "(host) setting up WinRM"
 
 cmd.exe /c winrm quickconfig -q
-cmd.exe /c winrm set "winrm/config" '@{MaxTimeoutms="1800000"}'
+cmd.exe /c winrm set "winrm/config" '@{MaxTimeoutms="3600000"}'
 cmd.exe /c winrm set "winrm/config/winrs" '@{MaxMemoryPerShellMB="1024"}'
 cmd.exe /c winrm set "winrm/config/service" '@{AllowUnencrypted="true"}'
 cmd.exe /c winrm set "winrm/config/client" '@{AllowUnencrypted="true"}'
